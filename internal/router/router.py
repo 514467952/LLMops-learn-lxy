414870c2cc9fb4fd3c82db29e7324c709f2b7b71
liuyuchen.dlrb <liuyuchen.dlrb@bytedance.com>
2025-12-30 10:53:36 +0800

    对话接口+接口校验

--- internal/router/router.py
+++ internal/router/router.py
@@ -31,6 +31,7 @@ class Router:
 
         # 2.将url与对应的控制器方法绑定
         bp.add_url_rule("ping", view_func=self.app_handler.ping)
+        bp.add_url_rule("app/completion", methods=["POST"], view_func=self.app_handler.completion)
 
         # 3.在应用上注册蓝图
         app.register_blueprint(bp)
