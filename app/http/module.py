19b82770d0c0e798206223c43333fdefc41e30ee
liuyuchen <liuyuchen3@xiaohongshu.com>
2026-03-25 21:52:20 +0800

    Flask-SQLAlchemy扩展的配置与使用

---
+++ app/http/module.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+"""
+@Time : 2026/3/25 21:47
+@Author : liuxiaoyu
+@File : module.py
+"""
+from flask_sqlalchemy import SQLAlchemy
+from injector import Module, Binder
+from internal.extension.database_extension import db
+
+
+class ExtensionModule(Module):
+    """扩展模块的依赖注入"""
+
+    def configure(self, binder: Binder) -> None:
+        binder.bind(SQLAlchemy, to=db)
